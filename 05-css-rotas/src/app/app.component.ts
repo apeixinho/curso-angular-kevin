4471dc006ec2b0c836192c7c176f9f7d0fff7b76
Kevin Gomes <kb.developer.br@gmail.com>
2022-08-24 14:25:39 -0400

    aula05: codigo implementado

--- 05-css-rotas/src/app/app.component.ts
+++ 05-css-rotas/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '05-css-rotas';
+  title = 'aula05';
+  @ViewChild('sidenav') sidenav?: MatSidenav;
+
+
+  close() {
+    this.sidenav?.close();
+  }
 }
--- 05-css-rotas/src/app/app.module.ts
+++ 05-css-rotas/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FirstComponent } from './first/first.component';
+import { Page404Component } from './page404/page404.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from "@angular/material/button";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Page404Component,
+    FirstComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
