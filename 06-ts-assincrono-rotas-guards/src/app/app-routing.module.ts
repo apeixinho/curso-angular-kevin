d23d8ec8914f041e96152fd0b60e1f63bfa15c3a
Kevin Gomes <kb.developer.br@gmail.com>
2022-08-31 14:07:10 -0400

    aula06-ts-assincrono-rotas-guards

---
+++ 06-ts-assincrono-rotas-guards/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { FirstComponent } from './first/first.component';
+import { Page404Component } from './page404/page404.component';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'first-component', component: FirstComponent },
+  { path: '**', component: Page404Component }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
